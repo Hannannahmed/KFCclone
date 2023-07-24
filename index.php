10df1fc727107beebb782ce8107f2fd359116a97
Hannannahmed <133767980+Hannannahmed@users.noreply.github.com>
2023-07-24 19:40:52 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,610 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KFC CLONE</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+</head>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Poppins:wght@700&family=Roboto&display=swap');
+  body{
+    background-color: black;
+  }
+  
+  
+  #butttonnavbar{
+  background-color: #1c1816;
+  color: white;
+  font-family: sans-serif;
+  font-size: 13px;
+  border-radius: 6px;
+  border: 2px solid #ea002a;
+    
+  }
+  #butttonnkfbutn{
+    background-color: #1c1816;
+  color: white;
+  font-family: sans-serif;
+  font-size: 13px;
+  border-radius: 6px;
+  }
+  .btnlogin{
+    background-color: #ea002a;
+    border-radius: 6px;
+    width: 70px;
+    border: none;
+    color: white;
+    height: 30px;
+  }
+  /* BEST DEAL CSS END HERE*/
+  .media-scroller {
+  overflow-x: auto;
+  white-space: nowrap;
+
+}
+
+.card {
+  display: inline-block;
+  width: 300px; /* Adjust card width as needed */
+  height: 400px; /* Adjust card height as needed */
+  margin-right: 10px; /* Adjust card margin as needed */
+  background-color: #1c1816; /* Card background color */
+  /* border: 1px solid #ccc; Card border */
+  border-radius: 5px; /* Card border radius */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Card box shadow */
+}
+#bestsellerh1{
+    font-size: 30px;
+    font-family:'poppins';
+    color: white;
+
+  }
+
+  #underline{
+  text-decoration: underline #ea002a solid 4px;
+  }
+  .cardheading{
+    color: white;
+    font-family: sans-serif,"poppins";
+    font-weight: bolder;
+
+  }
+  #ribbon{
+background-color: #ea002a;
+color: white;
+text-align: end;
+font-weight: bolder;
+font-size: 20px;
+border-top-left-radius: 40px;
+height: 35px;
+
+  }
+/* BEST DEAL CSS END HERE*/
+/* TOP DEAL CSS START HERE */
+.media-scroller {
+  overflow-x: auto;
+  white-space: nowrap;
+}
+
+.card1 {
+  display: inline-block;
+  width: 260px; /* Adjust card width as needed */
+  height: 430px; /* Adjust card height as needed */
+  margin-right: 10px; /* Adjust card margin as needed */
+  background-color: #1c1816; /* Card background color */
+  /* border: 1px solid #ccc; Card border */
+  border-radius: 5px; /* Card border radius */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Card box shadow */
+}
+
+.headingcard1{
+  font-family: 'poppins';
+  color: white;
+
+}
+.pragraphcard1{
+  color: white;
+  font-family: sans-serif;
+}
+.card1 img{
+  transition: 0.2s ease-in-out;
+}
+.card1 img:hover{
+ 
+      transform: scale(1.02);
+      transition: 0.2s ease-in-out;
+    }
+  
+    #btn{
+      text-align: center;
+    }
+    #btn button{
+         transition: 0.2s ease-in-out;
+      text-align: center;
+      background-color: #ea002a;
+  border: none;
+  border-radius: 10px;
+  text-align: center;
+  color: white;
+  font-family: 'poppins';
+  width: 200px;
+  padding: 4px;
+  font-size: 12px;
+  margin: 2px;
+  
+    }
+    #btn button:hover{
+      background-color: white;
+      color: #ea002a;
+      border: #ea002a 1px solid;
+      transition: 0.2s ease-in-out;
+
+    }
+/* TOP DEAL CSS END HERE */
+
+/*new section css start here*/
+.newsection img{
+    border-radius: 10px;
+  }
+  #btnnewsection{
+    background-color: #ea002a;
+    border: none;
+    border-radius: 6px;
+    font-family: 'poppins';
+    font-size: 20px;
+    color: white;
+    width: 200px;
+    height: 40px;
+   
+
+  }
+    .bgnewsection{
+    background-color: #1c1816;
+    border-radius: 10px;
+    
+
+  }
+
+/*new section css end here*/
+/* fotter css start here*/
+.parent{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  .child a {
+    text-decoration: none;
+    line-height: 40px;
+    font-family: sans-serif;
+    font-weight: bold;
+    font-size: 18px;
+    color: white;
+    
+
+  }
+  .child a:hover{
+    color: #ea002a;
+  }
+  .child a {
+    list-style: none;
+    
+
+  }
+  .footertext{
+    color: #ea002a;
+    font-size: 20px;
+
+  }
+  .bgfooter{
+    background-color: #1c1816;
+  }
+/* fotter css end here*/
+/*reorder buton css start here */
+.btnreorder button{
+  background-color: #ea002a;
+  border: none;
+  border-radius: 6px;
+  color: white;
+  font-size: 20px;
+  
+}
+/*reorder buton css end here */
+
+
+.media-scroller {
+  overflow-x: auto;
+  white-space: nowrap;
+}
+
+.card3 {
+  text-align: center;
+  display: inline-block;
+  width: 200px; /* Adjust card width as needed */
+  height: 220px; /* Adjust card height as needed */
+  margin-right: 15px; /* Adjust card margin as needed */
+  background-color: #1c1816; /* Card background color */
+  /* border: 1px solid #ccc; Card border */
+  border-radius: 5px; /* Card border radius */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Card box shadow */
+  border-top-left-radius: 400px;
+    /* border-bottom-left-radius: 200px; */
+  border-top-right-radius: 500px;
+  align-items: center;
+  border-end-start-radius: 500px;
+}
+.card3:hover{
+  transform: translateY(-10px);
+  transition: 0.2s ease-in-out;
+}
+.card3 p{
+  /* margin-left: 40px;
+  margin-bottom: 30px; */
+  font-family: sans-serif;
+ font-weight: bolder;
+ margin-left: 20px;
+ color: white;
+ text-decoration: underline #ea002a 3px solid;
+
+}
+#explore{
+    text-decoration: underline #ea002a 4px solid;
+  }
+  .exp{
+    font-family: 'poppins';
+  }
+</style>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-black">
+  <div class="container">
+    <a class="navbar-brand mx-5" href="#"><img src="image/logo.png"  alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><button id="butttonnavbar"><img src="image/deliverybutton.png" height="30px" width="30px" alt=""><span class="mt-5 mx-2">DELIVERY</span></button></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><button id="butttonnkfbutn"><img src="image/kfcbutton.png" height="30px" width="30px" alt=""><span class="mt-5 mx-2">PICKUP</span></button></a>
+        </li>
+       
+       
+       
+      </ul>
+      <form class="d-flex">
+     <img src="image/flycart.svg" width="50px" alt="">
+        <button class="btnlogin mt-2" type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!--navbar end here-->
+
+
+<!--slider start here-->
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <!-- <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button> -->
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/slider1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="image/slidr3.jpg" class="d-block w-100" alt="...">
+    </div>
+   
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!--slider end here-->
+<!--reorder button-->
+<div class="container btnreorder mt-3">
+  <button class="w-100">Reorder</button>
+</div>
+<!--reorder end button-->
+<!-- <style>
+  .gallery{
+    width: 1000px;
+    display: flex;
+    overflow-x: scroll;
+  }
+  .gallery div{
+    width: 200px;
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-gap: 20px;
+    padding: 10px;
+    flex: none;
+  }
+  .gallery div img{
+    width:200px;
+  }
+  .gallery::-webkit-scrollbar{
+    display: none;
+  }
+  .gallery-wrap{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10% auto;
+  }
+  #backbtn, #nextbtn{
+width: 20px;
+cursor: pointer;
+margin: 40px;
+  }
+  .bgcolor{
+    background-color: #1c1816;
+    border-top-left-radius: 300px;
+    border-bottom-left-radius: 200px;
+  border-top-right-radius: 300px;
+  border-end-start-radius: 300px;
+
+
+  }
+  .text{
+    text-decoration: underline #ea002a 2px solid;
+    
+  }
+
+</style>
+
+<div class="gallery-wrap">
+  <img src="image/back.png" id="backbtn" alt="">
+  <div class="gallery">
+    <div>
+      <div class="bgcolor">
+        <img src="image/slider1.png" alt="">
+   <h5 class=" text-white "> <span class="text">Everyday</span> value</h5>
+    </div>
+      <span><img src="image/zingerstactcombo.png" alt=""></span>
+      <span><img src="image/slider3.png" alt=""></span>
+     
+    </div>
+    <div>
+      <span><img src="image/fmaily festival.png" alt=""></span>
+      <span><img src="image/slider5.png" alt=""></span>
+      <span><img src="image/slider3.png" alt=""></span>
+    </div>
+  </div>
+  <img src="image/next.png" id="nextbtn" alt="">
+
+</div>
+<script>
+  let scrollContainer = document.querySelector(".gallery");
+  let backbtn=document.getElementById("backbtn");
+  let nextbtn=document.getElementById("nextbtn");
+
+  scrollContainer.addEventListener("wheel",(evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto" ;
+  });
+  nextbtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior = "smooth" ;
+    scrollContainer.scrollLeft += 900;
+  })
+  backbtn.addEventListener("click",()=>{
+    scrollContainer.style.scrollBehavior = "smooth" ;
+    scrollContainer.scrollLeft -= 900;
+  })
+</script> -->
+
+
+<!--EXPLORE MENU start HERE-->
+
+<div class="container p-0 mt-5 text-white">
+  <h2 class="exp mx-3 mt-5 "><span id="explore">EXPLORE</span> MENU</h2>
+<div class="media-scroller mt-5">
+  <div class="card3">
+    <img src="image/krucnhhh.png" width="160px" alt="">
+    <p class="text-center"> Everyday value</p>
+    <!-- Card Content Here -->
+  </div>
+  
+  <div class="card3">
+    <img src="image/zingerstactcombo.png" width="160px" alt="">
+    <p class="text-center"> Ala-Carte-&-Combos</p>
+    <!-- Card Content Here -->
+  </div>
+  
+  <div class="card3">
+    <img src="image/slider3.png" width="190px" alt="">
+    <p class="text-center"> Signature-Boxes</p>
+    <!-- Card Content Here -->
+  </div>
+  
+  <div class="card3">
+    <img src="image/fmaily festival.png" width="160px" alt="">
+    <p class="text-center"> Sharing</p>
+    <!-- Card Content Here -->
+  </div>
+  <div class="card3">
+    <img src="image/slider5.png" width="190px" alt="">
+    <p class="text-center"> Snack-&-Beverages</p>
+    <!-- Card Content Here -->
+  </div>
+  
+  <!-- Add more cards as needed -->
+</div>
+</div>
+<!--end here-->
+<!--best seller start here-->
+<div class="container mt-5">
+  <h1 id="bestsellerh1"><span id="underline">BEST </span>SELLERS</h1>
+ 
+  <div class="media-scroller mt-5">
+  <div class="card">
+    <!-- Card Content Here -->
+    <h4 class=" mx-2 mb-3 mt-3 cardheading ">Krunch burger</h4>
+    <p id="ribbon" class="p-0 m-0">Rs.270</p>
+    <img src="image/krucnhhh.png" width="100%" alt="">
+  </div>
+  <div class="card">
+    <!-- Card Content Here -->
+    <h4 class="mx-2 mb-3 mt-3 cardheading ">Hot Wings Buckets</h4>
+    <p id="ribbon" class="p-0 m-0">Rs.610</p>
+    <img src="image/hotwingsss.png" width="100%" alt="">
+  </div>
+  <div class="card">
+    <!-- Card Content Here -->
+    <h4 class="mx-2 mb-3 mt-3 cardheading ">Mighty Zinger</h4>
+    <p id="ribbon" class="p-0 m-0">Rs.700</p>
+    <img src="image/mightyzinger.png" width="100%" alt="">
+  </div>
+  <div class="card">
+    <!-- Card Content Here -->
+    <h4 class="mx-2 mb-3 mt-3 cardheading ">Krunch Combo</h4>
+    <p id="ribbon" class="p-0 m-0">Rs.520</p>
+    <img src="image/krunch combo.png" width="100%" alt="">
+  </div>
+  <!-- Add more cards as needed -->
+</div>
+</div>
+
+<!--best seller end here-->
+
+
+<!--Top deal start here-->
+<div class="container mt-5">
+  <h1 id="bestsellerh1"><span id="underline">TOP </span>DEALS</h1>
+  <div class="media-scroller mt-5">
+    <div class="card1">
+      <img class="img" src="image/twiester.png" width="100%" alt="">
+      <!-- Card Content Here -->
+<h6 class="headingcard1 mx-2 mt-2">Twister Combo</h6>
+<p class="pragraphcard1 mx-2">Twister + 1 Regular Fries + Regular <br> drink</p>
+<p class="pragraphcard1 mx-2">Rs.670</p>
+<div id="btn">
+  <button>+ ADD TO BUCKETS</button>
+  </div>
+  
+<!--card content end here-->
+    </div>
+    <div class="card1">
+      <img src="image/zingerstactcombo.png" width="100%" alt="">
+        <!-- Card Content Here -->
+<h6 class="headingcard1 mx-2 mt-2">Zinger Stack Combo </h6>
+<p class="pragraphcard1 mx-2">1 Zinger Stacker + 1 Regular Fries  <br>+1 Regular drink</p>
+<p class="pragraphcard1 mx-2">Rs.850</p>
+<div id="btn">
+  <button>+ ADD TO BUCKETS</button>
+  </div>
+  
+<!--card content end here-->
+    </div>
+    <div class="card1">
+      <img src="image/comoc.png" width="100%" alt="">
+     <!-- Card Content Here -->
+<h6 class="headingcard1 mx-2 mt-2">Crispy Duo Box </h6>
+<p class="pragraphcard1 mx-2">Turn up the fun with 5 pcs Hot & <br> CrispyChicken + 1 Large Fries +2...</p>
+<p class="pragraphcard1 mx-2">Rs.1250</p>
+<div id="btn">
+  <button>+ ADD TO BUCKETS</button>
+  </div>
+  
+<!--card content end here-->
+    </div>
+    <div class="card1">
+      <img src="image/fmaily festival.png" width="100%" alt="">
+      <!-- Card Content Here -->
+<h6 class="headingcard1 mx-2 mt-2">Family Festival 3</h6>
+<p class="pragraphcard1 mx-2">An ultimate meal for the fam.It <br> include for Zinger burger +4....</p>
+<p class="pragraphcard1 mx-2">Rs.2450</p>
+<div id="btn">
+<button>+ ADD TO BUCKETS</button>
+</div>
+<!--card content end here-->
+    </div>
+    <!-- Add more cards as needed -->
+  </div>
+  
+</div>
+<!--Top deal end here-->
+
+<!--NEW SECTION START HERE-->
+<div class="container newsection mt-5">
+  <div class="row">
+    <div class="col-md-6 mt-3">
+      <img src="image/pi9ckupfromstore.png" width="100%" alt="">
+    </div>
+    <div class="col-md-6 mt-3">
+      <img src="image/favoriteorder.png" width="100%" alt="">
+    </div>
+  </div>
+</div>
+
+<div class="container   newsection">
+  <div class="row">
+    <div class="col-md-6 mt-3">
+      <img src="image/kfcreorder.jpg" width="100%" alt="">
+    </div>
+    <div class="col-md-6 text-center text-white bgnewsection  mt-3 ">
+  <img  src="image/kfccc.jpg"  alt="">
+  <h5 >Adding 11 herbs and spices, Explore our menu and <br> add items to your cart</h5>
+  <button id="btnnewsection" class="mb-2" >EXPLORE MENU</button>
+    </div>
+  </div>
+</div>
+<!--NEW SECTION END HERE-->
+
+<!--Fotter start here-->
+<div class="container-fluid p-4 mt-5 bgfooter">
+<div class="container text-center mt-5 ">
+  <span class="me-3 "> <img src="image/logo.png"  alt=""></span>
+  <img  src="image/yt.png" width="60px" style="border-radius: 50px;" alt="">
+  
+  <img class="  mx-3" src="image/insta.png" width="40px" style="border-radius: 40px;" alt="">
+  <img   src="image/fbb.png" width="40px" style="border-radius: 50px;" alt="">
+</div>
+<div class="container mt-5">
+  <div class="parent">
+    <div class="child">
+      <a href=""><li>About Us</li></a>
+      <a href=""><li>Mitao Bhook</li></a>
+      <a href=""><li>Mitao Bhook-Scholarship</li></a>
+      <a href=""><li>Privacy  Policy</li></a>
+      <a href=""><li>Careers</li></a>
+    </div>
+    <div class="child">
+      <a href=""><li>Contact Us</li></a>
+      <a href=""><li>Store Locator</li></a>
+      <a href=""><li>Track Order</li></a>
+  
+    </div>
+    <div class="child">
+      <a href=""><li>Trems & Conditions </li></a>
+    </div>
+    <div class="child">
+   <img src="image/appstore.png" alt="">
+   <br>
+   <img class="mt-5" src="image/goofle.png" alt="">
+      <p class="footertext mt-4">Powered by</p>
+    <p style="text-decoration: underline;color: white;">SimpleX Technology Solution</p>  
+    </div>
+  </div>
+</div>
+</div>
+
+<!--Fotter end here-->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
